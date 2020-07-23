01071416e5a9e5c8aec75420a767ff00f45317dd
Dominik Lameter <dominik@lameter.com>
2020-07-23 12:12:10 -0500

    Removed library as it was for the wrong board.

--- src/main.c
+++ src/main.c
@@ -1,13 +1,15 @@
 #include "gd32vf103.h"
-#include "gd32vf103v_lcd_eval.h"
 
 int main() {
     // main entry file
 
     // Initialize the lcd
     exmc_lcd_init();
+    lcd_init();
 
     lcd_clear(LCD_COLOR_GREEN);
+
+    while(0) {};
 
     return 0;
 }
