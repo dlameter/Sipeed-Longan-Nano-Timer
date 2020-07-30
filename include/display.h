00b8f703c9af4179c0b4fdd4175661fbe80f82ac
Dominik Lameter <dominik@lameter.com>
2020-07-30 14:37:44 -0500

    Removed display in to free up memory space. Converted display_time to using less variables and to usding the new lcd library.

--- include/display.h
+++ include/display.h
@@ -1,7 +1,6 @@
 #ifndef DISPLAY_H
 #define DISPLAY_H
 
-void display_int(int x, int y, int value);
-void display_time(int x, int y, int value);
+void display_time(int x, int y, int value, uint16_t* buffer);
 
 #endif
--- src/display.c
+++ src/display.c
@@ -1,35 +1,18 @@
-#include "display.h"
-#include "lcd/lcd.h"
 #include "gd32vf103.h"
+#include "display.h"
+#include "lcd.h"
+#include "lcd_char.h"
 
-void display_int(int x, int y, int value) {
-    int base = '0';
-    int location = 0;
-    const int char_size = 8;
-
-    while (value != 0) {
-        int digit = value % 10;
-        LCD_ShowChar8(x - (char_size * location), y, base + digit, WHITE);
-        location++;
-        value /= 10;
-    }
-}
-
-void display_time(int x, int y, int value) {
-    int minute, minute_d1, minute_d2;
-    int second, second_d1, second_d2;
+void display_time(int x, int y, int value, uint16_t* buffer) {
+    int minute, second; 
     second = value / 1000;
     minute = second / 60;
     second = second % 60;
     minute = minute % 60;
-    minute_d2 = minute % 10;
-    minute_d1 = (minute / 10) % 10;
-    second_d2 = second % 10;
-    second_d1 = (second / 10) % 10;
 
-    LCD_ShowChar8(x, y, '0' + minute_d1, WHITE);
-    LCD_ShowChar8(x + 8, y, '0' + minute_d2, WHITE);
-    LCD_ShowChar8(x + 16, y, ':', WHITE);
-    LCD_ShowChar8(x + 24, y, '0' + second_d1, WHITE);
-    LCD_ShowChar8(x + 32, y, '0' + second_d2, WHITE);
+    draw_char(x, y, '0' + ((minute / 10) % 10), 0xffff, buffer);
+    draw_char(x + CHAR_WIDTH, y, '0' + (minute % 10), 0xffff, buffer);
+    draw_char(x + CHAR_WIDTH * 2, y, ':', 0xffff, buffer);
+    draw_char(x + CHAR_WIDTH * 3, y, '0' + ((second / 10) % 10), 0xffff, buffer);
+    draw_char(x + CHAR_WIDTH * 4, y, '0' + (second % 10), 0xffff, buffer);
 }
