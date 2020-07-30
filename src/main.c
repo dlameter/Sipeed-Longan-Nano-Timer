da4aeab01f78c6d9c9d01b73c60498c1dec7818f
Dominik Lameter <dominik@lameter.com>
2020-07-30 14:39:46 -0500

    Converted program to using new lcd library, created my own character display functions using it's framebuffer to allow larger text sizes, created larger digit and colon characters, removed background flashing when alarm is complete.

--- src/main.c
+++ src/main.c
@@ -1,18 +1,33 @@
 #include "gd32vf103.h"
 #include "display.h"
 #include "timer.h"
-#include "lcd/lcd.h"
+#include "lcd.h"
+#include "led.h"
+#include "bgcol.h"
+#include "lcd_char.h"
+#include "starfield.inl"
 
 #define TIMER_DURATION 90000
 
+// Creating global frambuffer for lcd library
+uint16_t g_fb[LCD_FRAMEBUFFER_PIXELS];
+
 int main() {
     // LCD init
-    u16 background_color = BLUE;
-    Lcd_Init();
-    LCD_Clear(background_color);
+    led_init();
+    lcd_init();
 
     // Init gpio for BOOT0 button that responds on PA8 after boot.
     gpio_init(GPIOA, GPIO_MODE_IN_FLOATING, GPIO_OSPEED_50MHZ, GPIO_PIN_8);
+
+    // Clear framebuffer
+    for (int i = 0; i < LCD_FRAMEBUFFER_PIXELS; i++) {
+        g_fb[i] = bgcol(i);
+    }
+
+    // Enable continuous framebuffer update
+    lcd_fb_setaddr(g_fb);
+    lcd_fb_enable();
 
     // State of BOOT0 button
     uint16_t button;
@@ -24,9 +39,10 @@ int main() {
     uint16_t triggered = 1;
     int timer_diff = TIMER_DURATION;
 
-    while(1) {
-        LCD_Clear(background_color);
+    // Position of timer text
+    int xbase = (LCD_WIDTH - 5 * CHAR_WIDTH) / 2, ybase = (LCD_HEIGHT - CHAR_HEIGHT) / 2;
 
+    while(1) {
         // If the timer is not triggered do a countdown
         if (!triggered) {
             timer_diff = timer_diff_in_milliseconds(start);
@@ -36,17 +52,6 @@ int main() {
                 timer_diff = TIMER_DURATION;
             }
         }
-        else {
-            if (background_color == BLUE) {
-                background_color = YELLOW;
-            }
-            else if (background_color == YELLOW) {
-                background_color = GREEN;
-            }
-            else if (background_color == GREEN) {
-                background_color = BLUE;
-            }
-        }
 
         // If the button is pressed restart the timer
         button = gpio_input_bit_get(GPIOA, GPIO_PIN_8);
@@ -54,14 +59,26 @@ int main() {
         if (button) {
             start = timer_start();
             triggered = 0;
-            background_color = BLUE;
+        }
+
+        // Update starfield from slmisc/gd32v-lcd
+        for (int i=0; i < NUM_STARS; i++)
+        {
+            Star* s = &g_stars[i];
+            if (s->p >= 0)
+                g_fb[s->p] = bgcol(s->p);
+            update_star(s, 10);
+            if (s->p >= 0)
+            {
+                int g = (0x7fff - s->z) >> 9;
+                if (g > 31) g = 31;
+                int r = g>>1;
+                g_fb[s->p] += (r << 11) | (g << 5) | r;
+            }
         }
 
         // display time till alarm goes off
-        display_int(8 * 16, 16, timer_diff);
-        display_time(8 * 12, 24, TIMER_DURATION - timer_diff);
-
-        delay_1ms(100);
+        display_time(xbase, ybase, TIMER_DURATION - timer_diff, g_fb);
     }
 
     return 0;
